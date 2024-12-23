34611a1419c9c0dbb9d4a798ac4976e512b6e108
caiogimenes <ciaonemegi@gmail.com>
2024-12-23 10:14:29 -0300

    exclui sql_utils, metodos foram incluidos no arquivo tse.py

--- tse.py
+++ tse.py
@@ -16,7 +16,9 @@ PACKAGE_SHOW_PATH = 'action/package_show'
 TAG_LIST_PATH = 'action/tag_list'
 GROUP_SHOW_PATH = 'action/group_show'
 OUTPUT_FOLDER = 'data'
+PATH = 'databases'
 
+        
 class TseClient():
     def __init__(self):
         self.session = requests.Session()
@@ -157,6 +159,47 @@ class TseClient():
         
         return f'Diretório {path} esvaziado' if not os.listdir(path) else f'Falha em remover arquivos de {path}'
     
+def df_to_sql(df: DataFrame, table_name: str, dbname: str):
+    if not os.path.exists(PATH):
+        os.makedirs(PATH)
+    # Conectar ao banco de dados
+    dbname = os.path.join(PATH, dbname)
+    conn = sqlite3.connect(dbname)
+    try:
+        # Nome da tabela será o nome do dataframe
+        df.to_sql(table_name, conn, if_exists='replace', index=False)  # Cria ou substitui a tabela com o nome do dataframe
+        print(f'Tabela "{table_name}" foi criada no banco de dados "{dbname}".')
+        return table_name
+    
+    except Exception as e:
+        print(f'Erro {e} ao carregar SQL. {type(e)}')
+        return None
+
+    finally:
+        # Fechar a conexão
+        conn.close()
+        
+def listar_tabelas(db_path: str):
+    # Conectar ao banco de dados
+    conn = sqlite3.connect(db_path)
+    try:
+        cursor = conn.cursor()
+        # Executar o comando para listar as tabelas
+        cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        tabelas = cursor.fetchall()
+        print("Tabelas no banco de dados:")
+        for tabela in tabelas:
+            print(f"- {tabela[0]}")
+        return [tabela[0] for tabela in tabelas]
+    
+    except Exception as e:
+        print(f"Erro ao listar tabelas: {e}")
+        return None
+    
+    finally:
+        # Fechar a conexão
+        conn.close()
+
 
 class TseAnalysis:
     def __init__(self):
